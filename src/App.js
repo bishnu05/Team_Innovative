dcf7ff69caac2fa0c838ebf1b642c0f9caa10534
Anowar Hossain <anowar3042@gmail.com>
2022-09-01 18:57:14 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Homepage } from "./Pages/Homepage";
+import { MainRoutes } from "./Pages/MainRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <MainRoutes />
+    </div>
+  );
+}
+
+export default App;
